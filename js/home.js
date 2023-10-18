4ef36d2b6b5853e7a388ba47d873269da4d467a2
hasyim404 <muhaisim7@gmail.com>
2023-10-19 01:34:31 +0700

    rename title page & disable inputs if checked

--- js/home.js
+++ js/home.js
@@ -10,3 +10,18 @@ window.addEventListener("click", function (e) {
   /*Fungsi Untuk menyembunyikan Dropdown List jika user mengklik diluar tombol dropdown*/
   if (!btn.contains(e.target)) profileDropdownList.classList.remove("active");
 });
+
+// Pembayaran donasi (anonim)
+function anonima() {
+  let checkbox = document.getElementById("checkbox");
+
+  if (checkbox) {
+    let inputs = document.getElementsByClassName("form-control");
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].disabled = checkbox.checked;
+    }
+  } else {
+    console.error("nothing");
+  }
+}
