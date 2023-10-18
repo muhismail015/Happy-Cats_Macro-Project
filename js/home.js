bf9eaceb381b54c7f0ef1eea48caaaa1e902af9d
hasyim404 <muhaisim7@gmail.com>
2023-10-19 00:24:13 +0700

    add profil & after login user profil

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-console.log("oke");
+// Navbar
+/*==================== Toggle Profile Navbar  ====================*/
+let profileDropdownList = document.querySelector(".prof-droplist");
+let btn = document.querySelector(".prof-dropdown-btn");
+
+/*Fungsi Untuk Menampilkan atau menyembunyikan Dropdown List*/
+const toggle = () => profileDropdownList.classList.toggle("active");
+
+window.addEventListener("click", function (e) {
+  /*Fungsi Untuk menyembunyikan Dropdown List jika user mengklik diluar tombol dropdown*/
+  if (!btn.contains(e.target)) profileDropdownList.classList.remove("active");
+});
